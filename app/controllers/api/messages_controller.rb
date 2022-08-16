51f682a5ecb18cba6cc9db3c7d73ac819bec05b7
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-08-16 17:10:16 +0100

    add api controller

---
+++ app/controllers/api/messages_controller.rb
@@ -0,0 +1,8 @@
+class Api::MessagesController < ApplicationController
+  def index
+    @message = Message.order(Arel.sql('RANDOM()')).first.text
+    render json: {
+      text: @message
+    }
+  end
+end
